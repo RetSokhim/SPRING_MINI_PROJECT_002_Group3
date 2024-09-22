3a7227ad935998875cba3be6074789fe2e71e971
sokhim <retsokhim21@gmail.com>
2024-09-22 10:57:11 +0700

    solved

--- keycloak-admin-client/src/main/java/org/example/controller/GroupController.java
+++ keycloak-admin-client/src/main/java/org/example/controller/GroupController.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/group")
 @CrossOrigin(origins = "http://localhost:9090")
+@SecurityRequirement(name = "keycloak_auth")
 public class GroupController {
 
     @GetMapping("/get/group")
--- task-service/src/main/java/org/example/TaskServiceApplication.java
+++ task-service/src/main/java/org/example/TaskServiceApplication.java
@@ -1,7 +1,11 @@
 package org.example;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @OpenAPIDefinition(
         servers = @Server(url = "/")
+)
+@SecurityScheme(
+        name = "keycloak_auth", // Name of the security scheme
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                clientCredentials = @OAuthFlow(
+                        tokenUrl = "http://localhost:8080/realms/master/protocol/openid-connect/token" // Token URL for Keycloak realm
+                )
+        )
 )
 public class TaskServiceApplication {
     public static void main(String[] args) {
