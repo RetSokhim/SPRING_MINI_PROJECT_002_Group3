d6e467a9ef81196fa52b973d330bb02517b323be
Seang <kheangngichseang@gmail.com>
2024-09-25 20:58:58 +0700

    add response to addUserToGroup and getAllUsersByGroupId

--- keycloak-admin-client/src/main/java/org/example/controller/GroupController.java
+++ keycloak-admin-client/src/main/java/org/example/controller/GroupController.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import org.example.model.Group;
 import org.example.model.User;
 import org.example.model.request.GroupRequest;
+import org.example.model.response.AddUserToGroupResponse;
 import org.example.model.response.ApiResponse;
+import org.example.model.response.UserListResponse;
 import org.example.service.GroupService;
 import org.keycloak.admin.client.resource.GroupResource;
 import org.springframework.http.HttpStatus;
@@ -81,22 +83,30 @@ public class GroupController {
     }
 
     @PostMapping("{groupId}/user/{userId}")
-    public ResponseEntity<String> addUserToGroup(@PathVariable UUID groupId, @PathVariable UUID userId){
+    public ResponseEntity<ApiResponse<AddUserToGroupResponse>> addUserToGroup(@PathVariable UUID groupId, @PathVariable UUID userId){
 //        groupService.addUserToGroup(groupId, userId);
-//        ApiResponse<Group> response = ApiResponse.<Group>builder()
-//                .status(HttpStatus.OK)
-//                .message("A group with ID " + groupId + " is deleted successfully!")
-//                .payload(null)
-//                .build();
-//        return ResponseEntity.status(HttpStatus.OK).body(response);
-
-        return ResponseEntity.status(HttpStatus.OK).body(groupService.addUserToGroup(groupId, userId));
+        ApiResponse<AddUserToGroupResponse> response = ApiResponse.<AddUserToGroupResponse>builder()
+                .status(HttpStatus.OK)
+                .message("The user with ID " + userId + " was added in group with ID " + groupId + " successfully!")
+                .payload(groupService.addUserToGroup(groupId, userId))
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+//        return ResponseEntity.status(HttpStatus.OK).body(groupService.addUserToGroup(groupId, userId));
     }
 
     @GetMapping("{groupId}/users")
-    public ResponseEntity<List<User>> getAllUsersByGroupId(@PathVariable String groupId){
+    public ResponseEntity<ApiResponse<UserListResponse>> getAllUsersByGroupId(@PathVariable String groupId){
 //        groupService.getAllUsersByGroupId(groupId);
-        return ResponseEntity.status(HttpStatus.OK).body(groupService.getAllUsersByGroupId(groupId));
+        ApiResponse<UserListResponse> response = ApiResponse.<UserListResponse>builder()
+                .status(HttpStatus.OK)
+                .message("All users that in group ID " + groupId + " are fetched successfully!")
+                .payload(groupService.getAllUsersByGroupId(groupId))
+                .build();
+        System.out.println(response);
+        System.out.println(groupService.getAllUsersByGroupId(groupId));
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+//        return ResponseEntity.status(HttpStatus.OK).body(groupService.getAllUsersByGroupId(groupId));
     }
 
 
--- keycloak-admin-client/src/main/java/org/example/service/userserviceimpl/GroupServiceImpl.java
+++ keycloak-admin-client/src/main/java/org/example/service/userserviceimpl/GroupServiceImpl.java
@@ -1,7 +1,6 @@
 package org.example.service.userserviceimpl;
 
 import jakarta.ws.rs.core.Response;
-import lombok.AllArgsConstructor;
 import org.example.exception.AlreadyExistException;
 import org.example.exception.NotFoundException;
 import org.example.model.Group;
@@ -9,7 +8,8 @@ import org.example.model.User;
 import org.example.model.mapper.GroupMapper;
 import org.example.model.mapper.UserMapper;
 import org.example.model.request.GroupRequest;
-import org.example.model.request.UserRequest;
+import org.example.model.response.AddUserToGroupResponse;
+import org.example.model.response.UserListResponse;
 import org.example.service.GroupService;
 import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
@@ -17,18 +17,14 @@ import org.keycloak.admin.client.resource.GroupResource;
 import org.keycloak.admin.client.resource.GroupsResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
-import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.GroupRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
@@ -136,20 +132,26 @@ public class GroupServiceImpl implements GroupService {
     }
 
     @Override
-    public String addUserToGroup(UUID groupId, UUID userId) {
+    public AddUserToGroupResponse addUserToGroup(UUID groupId, UUID userId) {
         UsersResource usersResource = keycloak.realm(realm).users();
         UserResource userResource = usersResource.get(String.valueOf(userId));
+        UserRepresentation userRepresentation = userResource.toRepresentation();
+        User user = UserMapper.toDto(userRepresentation);
         GroupsResource groupsResource = keycloak.realm(realm).groups();
         GroupRepresentation groupRepresentation = groupsResource.group(String.valueOf(groupId)).toRepresentation();
-
+        Group group = GroupMapper.toDto(groupRepresentation);
         userResource.joinGroup(groupRepresentation.getId());
-        return "user was added to group";
+//        return "user was added to group";
+        return new AddUserToGroupResponse(user, group);
     }
 
     @Override
-    public List<User> getAllUsersByGroupId(String groupId) {
+    public UserListResponse getAllUsersByGroupId(String groupId) {
         GroupResource groupResource = keycloak.realm(realm).groups().group(String.valueOf(groupId));
+        GroupRepresentation groupRepresentation = groupResource.toRepresentation();
+        Group group = GroupMapper.toDto(groupRepresentation);
         List<UserRepresentation> userRepresentationList = groupResource.members();
-        return userRepresentationList.stream().map(UserMapper::toDto).collect(Collectors.toList());
+        List<User> userList = userRepresentationList.stream().map(UserMapper::toDto).collect(Collectors.toList());
+        return new UserListResponse(group, userList);
     }
 }
