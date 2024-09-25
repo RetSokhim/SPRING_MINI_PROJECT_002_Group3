91b0eda518cbb74d5a19c0430be2837874d0a11e
sokmean.kao <sokmean.genz@gmail.com>
2024-09-25 11:14:42 +0700

    static secret

--- keycloak-admin-client/src/main/java/org/example/configuration/KeycloakClientConfig.java
+++ keycloak-admin-client/src/main/java/org/example/configuration/KeycloakClientConfig.java
@@ -12,7 +12,7 @@ import static org.keycloak.OAuth2Constants.CLIENT_CREDENTIALS;
 @Configuration
 public class KeycloakClientConfig {
 
-    @Value("${keycloak.credentials.secret}")
+    @Value("fRz2vJjIeM69NB06LbcujMWwZxzWYJlG")
     private String secretKey;
 
     @Value("${keycloak.resource}")
